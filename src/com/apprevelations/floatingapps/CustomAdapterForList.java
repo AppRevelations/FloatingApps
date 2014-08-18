546045a0cbd0cdad1b0e76534bfa13c6a22d289d
kgarg1995 <kgarg119@gmail.com>
2014-08-18 22:07:11 +0530

    removed unwanted import

--- src/com/apprevelations/floatingapps/CustomAdapterForList.java
+++ src/com/apprevelations/floatingapps/CustomAdapterForList.java
@@ -14,7 +14,6 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
